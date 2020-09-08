bdfe6d508fac389ec1c5470443460e23edc75ed5
ryoliao <ryodci@gmail.com>
2020-09-08 16:33:48 +0800

    FIX - JB-116 change test formatting

--- test/l10n_flutter_test.dart
+++ test/l10n_flutter_test.dart
@@ -12,11 +12,26 @@ void main() {
         fallbackLocale: LocaleProvider.desired('en-us'),
         startupLocale: LocaleProvider.desired('zh-tw'));
 
+    // getString with setup locale
     expect(I18n.resource.getString(12345), '黯陰羊老雞排');
+
+    // getString with fallback locale
     expect(I18n.resource.getString(23456), 'hahhhhh!?');
+
+    // format with setup locale
     expect(I18n.resource.format(34567, {'key': 123.456}), '超營養雞排 123.46');
+
+    // format with fallback locale
     expect(
         I18n.resource.format(45678, {'key': DateTime(2020, 9, 20, 12, 34, 56)}),
         '12:34:56 hehehe~');
+
+    // set locale to en-us
+    await I18n.switchLocale(LocaleProvider.desired('en-us'));
+
+    // getString with setup locale
+    expect(I18n.resource.getString(12345), 'first blood');
+    // format with setup locale
+    expect(I18n.resource.format(34567, {'key': 654.321}), '654.32 mdfk');
   });
 }
